197ff75e8c15f3134ded858c35d81515d8a0bd22
Lorenzo Teja <lorenzo.teja18@gmail.com>
2026-09-03 17:32:21 +0200

    Internal graphs refactoring and API changes
    The API has been changed, public methods have been set private and legacy code has been removed

--- doetools/graphs/__init__.py
+++ doetools/graphs/__init__.py
@@ -1,4 +1,3 @@
-from .design_plot_builder import DesignPlotOptions, build_design_plot
 from .plot_api_mixin import GraphsMixin
 
-__all__ = ["DesignPlotOptions", "GraphsMixin", "build_design_plot"]
+__all__ = ["GraphsMixin"]
