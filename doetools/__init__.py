5e9639d5ed4ecfffb8a2a6e8706353543a523867
Lorenzo Teja <lorenzo.teja18@gmail.com>
2026-09-05 09:56:39 +0200

    [Del] Deleted simulate responses from import

--- doetools/__init__.py
+++ doetools/__init__.py
@@ -20,7 +20,6 @@ from .utils import (
     ContinuousFactor,
     MixtureFactor,
     ModelTerms,
-    simulate_responses,
     suggest_design,
 )
 
@@ -37,7 +36,6 @@ __all__ = [
     "ImportDesign",
     "MixtureFactor",
     "ModelTerms",
-    "simulate_responses",
     "PlackettBurmanDesign",
     "SimplexCentroidDesign",
     "SimplexLatticeDesign",
